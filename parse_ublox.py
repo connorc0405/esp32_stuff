beaace98d3d87e8dc7964971c7adaa397b8b24d4
Connor Cumming <cumming.co@husky.neu.edu>
2021-03-18 21:09:48 -0400

    Stuff to help debug initial config message

--- parse_ublox.py
+++ parse_ublox.py
@@ -1,11 +1,22 @@
 from serial import Serial
-from pyubx2 import UBXReader
+from pyubx2 import UBXReader, UBXMessage
 
-stream = open('listening_gps_which_listens_board_115200_2.out', 'rb')
-# stream = open('board_gps_boot.txt', 'rb')
+stream = open('captures/listening_gps_which_listens_board_115200_2.out', 'rb')
+# stream = open('captures/board_gps_boot.txt', 'rb')
 # stream = Serial('/dev/tty.usbserial-145230', 115200, timeout=5)
 reader = UBXReader(stream)
 
-for (raw_data, parsed_data) in reader: print(parsed_data)
-# (raw_data, parsed_data) = reader.read()
-# print(parsed_data)
+# for (raw_data, parsed_data) in reader: print(parsed_data)
+
+
+for (_, parsed_data) in reader:
+	# if parsed_data.identity == "CFG-MSG":  # Probably telling us to enable NAV-SOL
+	if parsed_data.identity == "ACK-ACK":  # Probably telling us to enable NAV-SOL
+		# print(parsed_data.msgClass)
+		# print(parsed_data.msgID)
+		# ack = UBXMessage('ACK', 'ACK-ACK', GET)
+		# print(ack)
+		print(parsed_data.clsID)
+		
+	
+	# <UBX(ACK-ACK, clsID=CFG, msgID=CFG-MSG)>
