49e997ad01b201381820e0bffaf080aeb1b6e307
Connor Cumming <cumming.co@husky.neu.edu>
2021-04-14 14:33:20 -0400

    Clean up

--- laptop_prog.py
+++ laptop_prog.py
@@ -2,10 +2,20 @@ import datetime
 import socket
 import time
 
-
 from pyubx2 import UBXReader, UBXMessage, GET
 
-# Format is hhmmss.ss
+REMOTE_IP_ADDR = "10.10.10.1"
+REMOTE_PORT = 8080
+
+
+class GPSData():
+	def __init__(self, itow, lat, lon, h_msl, fix_type, num_sats):
+		self.itow = itow
+		self.lat = lat
+		self.lon = lon
+		self.h_msl = h_msl
+		self.fix_type = fix_type
+		self.num_sats = num_sats
 
 
 def send_msg(conn, pkt):
@@ -16,52 +26,37 @@ def send_msg(conn, pkt):
 		total_bytes_sent += bytes_sent
 
 
-def gen_msg(conn):
-	itow = 172399
-	lat=423362230
-	lon=-710865380
+def gen_msgs(conn, gps_data):
 	while True:
-		nav_sol = UBXMessage(
-			"NAV",
-			"NAV-SOL",
-			GET,
-			iTOW=itow,
-			fTOW=0,
-			week=2150,
-			gpsFix=3,
-			numSV=12
-			# ecefx=1530567000,
-			# ecefy=-4466994000,
-			# ecefz=273291000
-		)
 		nav_pvt = UBXMessage(
 			"NAV",
 			"NAV-PVT",
 			GET,
-			iTOW=itow,
-			lat=lat,
-			lon=lon,
-			height=10,
-			hMSL=1000,
-			fixType=3,
-			numSV=12
+			iTOW=gps_data.itow,
+			lat=gps_data.lat,
+			lon=gps_data.lon,
+			hMSL=gps_data.h_msl,
+			fixType=gps_data.fix_type,
+			numSV=gps_data.num_sats
 		)
-		# print("Sending NAV-SOL...")
-		# stream.write(nav_sol.serialize())
 		print("Sending NAV-PVT...")
 		conn.sendall(nav_pvt.serialize())
-		# print(len(nav_pvt.serialize()))
-		itow += 200
-		lat+=200
-		lon+=200
+		gps_data.itow += 200
+		# gps_data.lat+=200
+		# gps_data.lon+=200
 		time.sleep(.2)
 
 
 def main():
+	gps_data = GPSData(itow=172399, lat=423362230, lon=710865380, h_msl=1000, fix_type=3, num_sats=12)
+
+
 	conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	conn.connect(("10.10.10.1", 8080))
-	gen_msg(conn)
+	conn.connect((REMOTE_IP_ADDR, REMOTE_PORT))
+	print(f"Connected {REMOTE_IP_ADDR}:{REMOTE_PORT}")
+
+	gen_msgs(conn, gps_data)
 
 
 if __name__ == "__main__":
