036b4e67583d608103e44f131865f93d2b7a4ac4
Connor Cumming <cumming.co@husky.neu.edu>
2021-04-16 10:02:19 -0400

    Implemented keepalive

--- esp32_prog.py
+++ esp32_prog.py
@@ -3,24 +3,41 @@ import socket
 from machine import UART
 import time
 
+
 # Do I need to handle CFG messages?
 
-num=1
 
 def recv_ubx(conn_sock):
     recv_buf = bytearray()
+    try:
+        data = conn_sock.recv(1)  # Check if keepalive
+        print("received 1 byte")
+    except:
+        print("Socket timeout")
+        return ("fail", None)
+
+    if len(data) == 0:
+        print("Closed socket")
+        return ("fail", None)
+    elif data == b'A':
+        return ("keepalive", None)
+    print("Not A")
+    print(repr(data))
+    recv_buf.extend(data)
+
     while len(recv_buf) < 6:  # Idx 4-5 contain payload length
         try:
             data = conn_sock.recv(6-len(recv_buf)) # TODO make sure we get 6!!!
         except:
             print("Socket timeout")
-            return None
+            return ("fail", None)
 
         if len(data) == 0:
             print("Closed socket")
-            return None
+            return ("fail", None)
         
         recv_buf.extend(data)
+
     payload_len = int.from_bytes(recv_buf[4:], 'little')
 
     while len(recv_buf) < payload_len + 8:  # 6 bytes + payload + 2 bytes checksum
@@ -28,15 +45,15 @@ def recv_ubx(conn_sock):
             data = conn_sock.recv(payload_len + 8 - len(recv_buf))
         except:
             print("Socket timeout")
-            return None
+            return ("fail", None)
         
         if len(data) == 0:
             print("Closed socket")
-            return None
+            return ("fail", None)
         
         recv_buf.extend(data)
 
-    return bytes(recv_buf)
+    return ("newpkt", bytes(recv_buf))
 
 
 ap = network.WLAN(network.AP_IF)
@@ -60,13 +77,21 @@ while True:
     conn_sock, _ = listen_sock.accept()
     conn_sock.settimeout(5)
     print("New connection")
+    num=1
+    cur_pkt = None
     while True:
-
-        print("Packet " + str(num))
-        ubx_pkt = recv_ubx(conn_sock)
-        if ubx_pkt is None: # Socket was closed
+        print("Here")
+        status, new_pkt = recv_ubx(conn_sock)
+        if status == "fail": # Socket was closed
             conn_sock.close()
             break
-
-        num+=1
-        uart.write(ubx_pkt)
+        elif status == "keepalive":
+            print("Keepalive")
+        elif status == "newpkt":  # Good packet
+            print("Packet " + str(num))
+            cur_pkt = new_pkt
+            print(type(cur_pkt))
+            num+=1
+        else:
+            raise Exception("IDK")
+        uart.write(cur_pkt)
--- laptop_prog.py
+++ laptop_prog.py
@@ -11,107 +11,123 @@ REMOTE_PORT = 8080
 
 
 class GPSData():
-	def __init__(self, itow, lat, lon, h_msl, fix_type, num_sats):
-		self.itow = itow
-		self.lat = lat
-		self.lon = lon
-		self.h_msl = h_msl
-		self.fix_type = fix_type
-		self.num_sats = num_sats
-		self.lock = threading.Lock()
+    def __init__(self, itow, lat, lon, h_msl, fix_type, num_sats):
+        self.itow = itow
+        self.lat = lat
+        self.lon = lon
+        self.h_msl = h_msl
+        self.fix_type = fix_type
+        self.num_sats = num_sats
+        self.updated = True  # Send first one correctly
+        self.lock = threading.Lock()
 
 
 def send_msg(conn, pkt):
-	len_pkt = len(pkt)
-	total_bytes_sent=0
-	while total_bytes_sent < len_pkt:
-		bytes_sent = conn.send(pkt)
-		total_bytes_sent += bytes_sent
+    len_pkt = len(pkt)
+    total_bytes_sent=0
+    while total_bytes_sent < len_pkt:
+        bytes_sent = conn.send(pkt)
+        total_bytes_sent += bytes_sent
 
 
 def gen_msgs(conn, gps_data):
-	num = 1
-	while True:
-		gps_data.lock.acquire()
-		nav_pvt = UBXMessage(
-			"NAV",
-			"NAV-PVT",
-			GET,
-			iTOW=gps_data.itow,
-			lat=gps_data.lat,
-			lon=gps_data.lon,
-			hMSL=gps_data.h_msl,
-			fixType=gps_data.fix_type,
-			numSV=gps_data.num_sats
-		)
-		gps_data.itow += 200
-		gps_data.lock.release()
-		print(f"Sending NAV-PVT {num}")
-		num+=1
-		send_msg(conn, nav_pvt.serialize())
-		time.sleep(.2)
+    num = 1
+    while True:
+        gps_data.lock.acquire()
+        updated = gps_data.updated
+        gps_data.lock.release()
+
+        if updated:
+            gps_data.lock.acquire()
+            nav_pvt = UBXMessage(
+                "NAV",
+                "NAV-PVT",
+                GET,
+                iTOW=gps_data.itow,
+                lat=gps_data.lat,
+                lon=gps_data.lon,
+                hMSL=gps_data.h_msl,
+                fixType=gps_data.fix_type,
+                numSV=gps_data.num_sats
+            )
+            gps_data.itow += 200
+            gps_data.updated = False
+            gps_data.lock.release()
+            print(f"Sending NAV-PVT {num}")
+            num+=1
+            send_msg(conn, nav_pvt.serialize())
+        else:  # Send keep-alive
+            print(f"Sending keepalive")
+            send_msg(conn, b'A')
+        time.sleep(.2)
 
 
 def key_monitor(gps_data):
-	while True:
-		char = input()
-		print(repr(char))
-		if char == 'w':  # increase latitude
-			print("north")
-			gps_data.lock.acquire()
-			gps_data.lat += 200
-			gps_data.lock.release()
+    while True:
+        char = input()
+        print(repr(char))
+        if char == 'w':  # increase latitude
+            print("north")
+            gps_data.lock.acquire()
+            gps_data.lat += 200
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		elif char == 's':  # decrease latitude
-			print("south")
-			gps_data.lock.acquire()
-			gps_data.lat -= 200
-			gps_data.lock.release()
+        elif char == 's':  # decrease latitude
+            print("south")
+            gps_data.lock.acquire()
+            gps_data.lat -= 200
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		elif char == 'a':  # increase longitude
-			print("east")
-			gps_data.lock.acquire()
-			gps_data.lon += 200
-			gps_data.lock.release()
+        elif char == 'a':  # increase longitude
+            print("east")
+            gps_data.lock.acquire()
+            gps_data.lon += 200
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		elif char == 'd':  # decrease longitude
-			print("west")
-			gps_data.lock.acquire()
-			gps_data.lon -= 200
-			gps_data.lock.release()
+        elif char == 'd':  # decrease longitude
+            print("west")
+            gps_data.lock.acquire()
+            gps_data.lon -= 200
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		elif char == '[':  # increase height
-			print("up")
-			gps_data.lock.acquire()
-			gps_data.h_msl += 1000
-			gps_data.lock.release()
+        elif char == '[':  # increase height
+            print("up")
+            gps_data.lock.acquire()
+            gps_data.h_msl += 1000
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		elif char == '\'':  # decrease height
-			print("down")
-			gps_data.lock.acquire()
-			gps_data.h_msl -= 1000
-			gps_data.lock.release()
+        elif char == '\'':  # decrease height
+            print("down")
+            gps_data.lock.acquire()
+            gps_data.h_msl -= 1000
+            gps_data.updated = True
+            gps_data.lock.release()
 
-		else:
-			print("Unrecognized Key")
+        else:
+            print("Unrecognized Key")
 
 
 def main():
-	gps_data = GPSData(itow=172399, lat=423362230, lon=-710865380, h_msl=1000, fix_type=3, num_sats=12)
+    gps_data = GPSData(itow=172399, lat=423362230, lon=-710865380, h_msl=1000, fix_type=3, num_sats=12)
 
-	input_thread = threading.Thread(target=key_monitor, args=(gps_data,))
-	input_thread.start()
+    input_thread = threading.Thread(target=key_monitor, args=(gps_data,))
+    input_thread.start()
 
-	conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	conn.connect((REMOTE_IP_ADDR, REMOTE_PORT))
-	print(f"Connected {REMOTE_IP_ADDR}:{REMOTE_PORT}")
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    conn.connect((REMOTE_IP_ADDR, REMOTE_PORT))
+    print(f"Connected {REMOTE_IP_ADDR}:{REMOTE_PORT}")
 
-	gen_msgs(conn, gps_data)
+    gen_msgs(conn, gps_data)
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
 
